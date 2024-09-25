15839cc1a195ba54128cdfd68dbbdd1b4aed59db
NahiaraCuella <nahiaracuella16@gmail.com>
2024-09-25 10:58:26 -0300

    Arme la clase jugador

--- jugador.py
+++ jugador.py
@@ -2,4 +2,15 @@ class Jugador:
     def __init__(self,nombre,fichas=5):
         self.nombre = nombre
         self.fichas = fichas 
-   
+    def __repr__(self):
+        return f"{self.nombre}, {self.fichas} fichas"
+    def darFicha(self, cuantas=1):
+        self.fichas += cuantas
+    def sacarFicha(self, cuantas=1):
+         if cuantas > self.fichas:
+            raise ValueError("No puedes sacar más fichas.")
+         self.fichas -= cuantas
+    def tieneFicha(self,cuantas=1):
+      return self.fichas >= cuantas
+    def sinFichas(self):
+      return self.fichas == 0
